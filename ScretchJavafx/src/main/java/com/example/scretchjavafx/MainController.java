94328f541b9dfe05f7751e60d9e4274c82264f5e
abdullah-dev5 <imabdullah.tdm@gmail.com>
2023-05-05 19:49:47 +0500

    Create MainController.java

---
+++ ScretchJavafx/src/main/java/com/example/scretchjavafx/MainController.java
@@ -0,0 +1,65 @@
+package com.example.scretchjavafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+
+public class MainController {
+
+
+
+    @FXML
+    private Label result;
+private long number1=0;
+private String operator="";
+private boolean start = true;
+private Modal model = new Modal();
+@FXML
+    public void processNumbers(ActionEvent event) {
+
+    if(start)
+    {
+        result.setText("");
+        start =false;
+    }
+    String value =((Button)event.getSource()).getText();
+    result.setText(result.getText()+value);
+
+
+
+
+    }
+    @FXML
+    public void processOperator(ActionEvent event) {
+
+        String value =((Button)event.getSource()).getText();
+        if(!value.equals("="))
+        {
+
+           if(!operator.isEmpty())
+           {
+               return;
+           }
+           operator=value;
+           number1=Long.parseLong(result.getText());
+           result.setText("");
+        }
+        else {
+            if(operator.isEmpty())
+            {
+               return;
+            }
+            long number2 = Long.parseLong(result.getText());
+            float output =   model.calculate(number1,number2,operator);
+            result.setText(String.valueOf(output));
+            operator="";
+            start=true;
+        }
+
+    }
+
+
+}
